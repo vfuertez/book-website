4291949135e6069ca3a330700ad689a9ff299ca8
Vincent Fuertez <vfuertez@gmail.com>
2023-01-14 03:57:25 -0500

    added updateAction function in actions.js

--- src/actions.js
+++ src/actions.js
@@ -2,6 +2,7 @@ import { redirect } from 'react-router-dom'
 
 const URL = 'https://book-api-q5e1.onrender.com'
 
+/// Create ////
 export const createAction = async ({ request }) => {
   const formData = await request.formData()
 
@@ -16,6 +17,27 @@ export const createAction = async ({ request }) => {
     },
     body: JSON.stringify(newBook),
   })
-
   return redirect('/')
+}
+
+//// Update ////
+
+export const updateAction = async ({request, params}) => {
+    // get data from form
+    const formData = await request.formData()
+    // set up our new person to match schema
+    const updatedBook = {
+        website: formData.get("website"),
+        url: formData.get("url"),
+    }
+    // Send new person to our API
+    await fetch(URL + "/book/" + params.id, {
+        method: "put",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(updatedBook)
+    })
+    // redirect to index
+    return redirect("/")
 }
