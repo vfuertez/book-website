5e1314f89fcab8cd9b05e5a79ab60c150de3b12e
Vincent Fuertez <vfuertez@gmail.com>
2023-01-14 03:43:39 -0500

    added loader data in Show.js

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,17 @@
+import { useLoaderData } from "react-router-dom"
+
 function Show(props) {
-    return <h1>SHow</h1>
+
+    const book = useLoaderData()
+
+    return (
+        <div>
+            <h1>{book.website}</h1>
+        <h2><a href ={book.url}>{book.url}</a></h2>
+        </div>
+    )
   }
   
+
+
   export default Show
