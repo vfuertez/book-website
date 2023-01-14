d999ab3fb060fbaedf6dd1b0d42b5b62d3a2cc8e
Vincent Fuertez <vfuertez@gmail.com>
2023-01-14 03:34:16 -0500

    imported show route loader in router.js

--- src/router.js
+++ src/router.js
@@ -8,12 +8,13 @@ import Show from './pages/Show'
 import Index from './pages/Index'
 import { bookLoader } from './loaders'
 import { createAction } from './actions'
+import { selectedBook } from './loaders'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Index />} loader={bookLoader} />
-      <Route path=":id" element={<Show />} />
+      <Route path=":id" element={<Show />} loader={selectedBook} />
       <Route path="create" action={createAction}/>
       <Route path="update/:id" />
       <Route path="delete/:id" />
